10ccef3d28b84c6274036f613d60a1bbbb0d361a
jeffj <jenkinsjeffrey@gmail.com>
2015-05-11 00:34:25 -0700

    adding auth

--- app/verts/routes/index.js
+++ app/verts/routes/index.js
@@ -6,7 +6,6 @@
 // Note: We can require users, articles and other cotrollers because we have
 // set the NODE_PATH to be ./app/controllers (package.json # scripts # start)
 
-//var users = require('../app/users/controllers/users');
 var verts = require('../controllers/verts');
 var express = require('express');
 var config = require('../../../config/config');
@@ -17,7 +16,7 @@ var verts = mongoose.model('Verts');
 
 // var comments = require('../app/controllers/comments');
 // var tags = require('../app/controllers/tags');
-//var auth = require('../../../config/middlewares/authorization');
+var auth = require('../../../config/middlewares/authorization');
 
 /**
  * Expose routes
@@ -29,9 +28,6 @@ module.exports = function (app, passport, auth) {
    * Route middlewares
    */
 
-  var articleAuth = [auth.requiresLogin, auth.article.hasAuthorization];
-  var commentAuth = [auth.requiresLogin, auth.comment.hasAuthorization];
-
-  crudUtils.initRoutesForModel(app, verts,'/verts')
+  crudUtils.initRoutesForModel(app, verts, auth, '/verts')
   
 }
