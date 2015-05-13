2b7c5603a17e0903a09a79fa92dd318184a1b3fb
jeffj <jenkinsjeffrey@gmail.com>
2015-05-13 02:42:23 -0700

    switching to index js.

---
+++ app/main/frontend/index.js
@@ -0,0 +1,53 @@
+/**
+npm * Copyright (c) 2014, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
+var Link = Router.Link;
+
+//Import the modules of the different app components
+var Verts = require('../../verts/frontend/app');
+
+var Home = React.createClass({
+  render: function () {
+    return <div><Link to="verts">Dashboard</Link><h2>Home</h2></div>;
+  }
+});
+
+
+
+
+// declare our routes and their hierarchy
+var routes = (
+  <Route handler={App} location="history" strict="false">
+    <Route name="verts" path="/verts" handler={Verts}/>
+    <Route name="home" path="/" handler={Home}/>
+  </Route>
+);
+
+// Execute the app
+var RouteHandler = Router.RouteHandler;
+
+
+var App = React.createClass({
+  render:function() {
+    return (
+      <div>
+        <h1>App</h1>
+        <RouteHandler/>
+      </div>
+    )
+  }
+});
+
+
+Router.run(routes, Router.HistoryLocation, function(Root){
+  React.render(<Root/>, document.getElementById('todoapp'));
+});
